12e74749df756304132dbfa7f6fbf7e1c54be0c8
Your Name <you@example.com>
2013-11-06 18:42:36 +0800

    shell script

--- deployment_tools/rsync_prallel_to_host.sh
+++ deployment_tools/rsync_prallel_to_host.sh
@@ -1,1 +1,43 @@
 #!/bin/sh
+
+#
+#
+
+HOST=
+RSYNC_MODULE=
+USER=
+PASSWD_FILE=
+
+
+if [ $# -eq 0 ]
+then
+	echo " argument !"
+	exit 1
+fi
+while test $# -gt 0
+do
+	case $1 in
+        -h | -H)
+	 shift
+	HOST="$1"
+	;;
+	
+	-m| -M)
+	shift
+	RSYNC_MODULE="$1"
+	;;
+	
+	-p |-M)
+	shift
+	PASSWORD_FILE="$1"
+	;;
+	
+	*)
+	echo " error"
+	break
+	;;
+
+	esac
+	shift
+done
+echo $HOST $RSYNC_MODULE $USER $PASSWD_FILE
