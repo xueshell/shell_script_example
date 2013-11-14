431963a2aa2c78cd193596d6a50069846dcb6280
Your Name <you@example.com>
2013-11-14 18:45:49 +0800

    curl

--- curl_array_site_check.sh
+++ curl_array_site_check.sh
@@ -1,24 +1,21 @@
 #! /bin/sh
 
-website[0]='zufang.hfhouse.com/chuzu/'　　#网站1
-mobile[0]='15955159890'　　　　　　　　　　 #对应网站1 手机号码
+website[0]='zufang.hfhouse.com/chuzu/'
+mobile[0]='15955159890'
+website[1]='oldhouse.hfhouse.com/chushou/'
+mobile[1]='15955159890'
 
-website[1]='oldhouse.hfhouse.com/chushou/' #同上2
-mobile[1]='15955159890'                    #同上2
-
-#以此类推，若网站过多 则可进行文件，或数据库读取方式
-
-length=${#website[@]}   #获取网站总数量
-for ((i=0; i<$length; i++)) #循环执行
+length=${#website[@]} 
+for ((i=0; i<$length; i++)) 
 do
-   status=$(curl -I -m 10 -o /dev/null -s -w %{http_code} ${website[$i]})   #CURL 获取http状态码
-   if [ "$status"x != "200"x ]; then      #检测是否为 200(正常)
+   status=$(curl -I -m 10 -o /dev/null -s -w %{http_code} ${website[$i]})  
+   if [ "$status"x != "200"x ]; then      #
     echo ${website[$i]} '=>' $status  
-    #php /htdoc/jk/shell_monitor.php ${mobile[$i]} ${website[$i]}'=>AccessError!'  #执行PHP文件(使用PHP主要为了采用第三方短信类库，此处也可进行Mail发送)
-   fi #结束if
-done #结束 do
+    #
+   fi #
+done #
 
-#执行：crontab -e
-#写入内容：5 * * * * /shell path
-#(上面是5分钟执行一次
+#crontab -e
+#5 * * * * /shell path
+
 
