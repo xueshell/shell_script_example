a7df20ad8579e2bd1e037a370248fc3b0036eaec
Your Name <you@example.com>
2013-11-15 16:47:12 +0800

    check website with curl finish

--- curl_array_site_check.sh
+++ curl_array_site_check.sh
@@ -4,14 +4,16 @@ QB_PATH="/data0/qb_conf_backup/10.55.37.64/quickbuild_conf/"
 NUM=0
 
 
-website[0]='http://10.55.37.64'
+website[0]=''
+QBHOST="http://10.55.37.64"
 mobile[0]='15955159890'
 #website[1]='oldhouse.hfhouse.com/chushou/'
 #mobile[1]='15955159890'
 
 for LINE in $( ls ${QB_PATH} )
 do
-	echo $NUM  $LINE
+	echo $NUM  $LINE ${website[$NUM]}
+	website["$NUM"]="$QBHOST:$LINE/signin"
 	NUM=$((NUM+1));
 done
 
@@ -22,8 +24,11 @@ for ((i=0; i<$length; i++))
 do
    status=$(curl -I -m 10 -o /dev/null -s -w %{http_code} ${website[$i]})  
    if [ "$status"x != "200"x ]; then      #
-    echo ${website[$i]} '=>' $status  
-    #
+  	 echo ${website[$i]} '=>' $status  
+   #
+  else 
+	echo ${website[$i]} is OK!
+    
    fi #
 done #
 
