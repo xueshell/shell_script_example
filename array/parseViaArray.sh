b24033967567d81c59dd1fb35bf7e6a0332b3eaf
lixin <lixin@staff.sina.com.cn>
2014-04-30 12:04:06 +0800

    array

---
+++ array/parseViaArray.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#cookbook filename:parseViaArray
+#find the file size
+#use an array to parse the ls -l output into words
+
+LSL=$(ls -ld $1)
+declare -a MYRA
+MYRA=($LSL)
+echo the file $1 is ${MYRA[4]} bytes.
+
+for (( i=0; i<${#MYRA}; i++ ))
+do
+	echo ${MYRA[i]};
+	tr -d '\n';
+	continue
+done
