913419b151ab57358d6d1c50afd2c6f77d388ca1
Uahoo <uahoo@sina.com>
2014-05-12 17:30:15 +0800

    monitor disk space

--- monitor_dsk_mem_net/monitor_or_watch_the_disk_space.sh
+++ monitor_dsk_mem_net/monitor_or_watch_the_disk_space.sh
@@ -17,11 +17,13 @@
     ALERT=90
     df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
     do
-    #echo $output
+    echo $output
     usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1 )
     partition=$(echo $output | awk '{ print $2 }' )
     if [ $usep -ge $ALERT ]; then
     echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
     mail -s "Alert: Almost out of disk space $usep" $ADMIN
+    else
+	echo "The disk space is  available !"
     fi
     done
