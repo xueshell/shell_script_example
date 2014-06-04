9b70cc64d0f9e20ab05f7e8b0350d593391a3c3e
Uahoo <uahoo@sina.com>
2014-06-04 17:09:07 +0800

    backup

---
+++ release_development/Git-Deploy-by-FTP/dp-git.sh
@@ -0,0 +1,209 @@
+#!/bin/sh
+
+#### FUNCTIONS ####
+function usage {
+	printf "usage: dp.sh -p <project-name> -d <commit-sha> [--not-simulate]\n"
+	printf "\t -p, --project  \tThe name of the project to deploy (look at the configuration file)\n"
+	printf "\t -d, --deploy   \tThe commit you want to deploy \n"
+	printf "\t --not-simulate \tReally deploy, it will not simulate!\n"
+    printf "\t --no-colors    \tDisable colors layout\n"
+}
+
+######### Start script ########
+echo "Production deployment script (ver. 0.2)"
+
+if [ "$1" == "" ]; then
+    usage
+	exit 1
+fi
+
+# Check parameters and Set defaults
+SIMULATION=0
+USE_COLOR=1
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --project )
+							shift
+							PROJECT_NAME=$1
+							;;
+        -d | --deploy )
+							shift
+							DEPLOY_SELECTED=$1
+							;;
+        --not-simulate )
+							SIMULATION=1
+							;;
+        --no-colors )
+							USE_COLOR=0
+							;;
+        -h | --help )
+							usage
+							exit;;
+        * )
+							usage
+							exit 1
+    esac
+    shift
+done
+
+# SET Script Directory (so you can call it from anywhere)
+BASEDIR=$(dirname $0)
+echo == $BASEDIR ==
+cd $BASEDIR
+
+# Check NO-COLORS parameter (disable colors)
+if [ "$USE_COLOR" == 0 ]
+then
+    COLOR_ALERT=""
+    COLOR_ERROR=""
+    COLOR_MSG=""
+    COLOR_RESET=""
+    COLOR_LOG=""
+else
+
+#### COLOR SETTINGS ####
+    COLOR_ALERT=$(tput setaf 5) # Fuchsia
+    COLOR_ERROR=$(tput setaf 1) # Red
+    COLOR_MSG=$(tput setaf 7)   # White
+    COLOR_LOG=$(tput setaf 3)   # Yellow
+    COLOR_RESET=$(tput sgr0)    # Reset any color
+fi
+
+# Simulation alert
+if [ "$SIMULATION" == 0 ]
+then
+echo $COLOR_ALERT"** SIMULATION MODE ENABLED **"
+fi
+
+# Check if the config file selected EXISTS and include it
+if [[ -z "$PROJECT_NAME" ]]
+then
+	echo $COLOR_ERROR"Please, select a project name to use the correct config file!"
+	exit 0
+else
+	if [ -f .dp-config-$PROJECT_NAME ]
+	then	
+		echo ===$PWD====
+		#cat  .dp-config-$PROJECT_NAME
+		source  ./.dp-config-$PROJECT_NAME
+	else
+		echo $COLOR_ERROR"The config file '.dp-config-$PROJECT_NAME' does NOT EXISTS!"
+		exit 0
+	fi
+fi
+
+##
+#if [ -d $REPO_CLONED_DIR ]
+#then
+#	rm -Ir $REPO_CLONED_DIR
+#	echo  $REPO_CLONED_DIR
+#fi
+#ls -l  $REPO_CLONE_DIR	
+
+rm -fR $EXPORTED_DIR/source
+rm -fR $EXPORTED_DIR/t
+#git clone $PROJECT_REPO_DIR $REPO_CLONED_DIR
+
+# Check if Deploy commit EXISTS
+cd $PROJECT_REPO_DIR
+C_DEPLOY=`git rev-list $DEPLOY_SELECTED | head -n 1`
+echo ==c_deploy==$C_DEPLOY====
+if [[ -z "$C_DEPLOY" ]]
+then
+	echo $COLOR_ERROR"The commit '$DEPLOY_SELECTED' does NOT EXISTS in the repo: \n$PROJECT_REPO_DIR!"
+	exit 0
+else
+	echo $COLOR_MSG"Commit to deploy: $C_DEPLOY"
+fi
+
+# Check production tag
+echo "Check for '$PRODUCTION_TAG_NAME' tag..."
+C_PRODUCTION=`git rev-list $PRODUCTION_TAG_NAME | head -n 1`
+if [[ -z "$C_PRODUCTION" ]]
+then
+	echo $COLOR_ERROR"The tag 'production' does NOT EXISTS in the repo: \n$PROJECT_REPO_DIR!"
+	exit 0
+else
+	echo $COLOR_MSG"Production environment is on commit: $C_PRODUCTION"
+fi
+
+echo $COLOR_MSG"Clean Exported directory..."$COLOR_RESET
+#rm -fR $EXPORTED_DIR
+
+echo $COLOR_MSG"Clone Git Repository..."$COLOR_RESET
+#git clone --recursive file://$PROJECT_REPO_DIR$PROJECT_NAME $REPO_CLONED_DIR
+git clone file://$PROJECT_REPO_DIR $REPO_CLONED_DIR
+cd $REPO_CLONED_DIR
+
+echo $COLOR_MSG"Get diff between commits: $C_PRODUCTION - $C_DEPLOY "$COLOR_RESET
+echo ==c_deploy===$C_DEPLOY=====
+echo ===filename==$FILENAME_DIFF====
+echo ====" git diff --name-status $C_PRODUCTION $C_DEPLOY > $EXPORTED_DIR/$FILENAME_DIFF " ======
+git diff --name-status $C_PRODUCTION $C_DEPLOY >$EXPORTED_DIR/$FILENAME_DIFF
+
+echo $COLOR_MSG"Checking out $C_DEPLOY ..."$COLOR_RESET
+git checkout $C_DEPLOY
+
+echo $COLOR_MSG"Start to read the diff files..."$COLOR_RESET
+# Start to Deploy Production environment
+COUNTER=0
+while read row; do
+	COUNTER=$[COUNTER + 1]
+	read -r f_status f_path <<< "$row"
+	echo "$COUNTER - \c" # write the Row line
+	# Check if it Is a file
+	case $f_status in
+
+		# STATUS: M (Modified), A (Added)
+		[AM] )
+			echo $COLOR_LOG"Upload... ($f_status): $f_path";
+	#
+		         DIR=$( dirname $f_path )
+		         echo --dir---$DIR----
+			 cd $EXPORTED_DIR
+			 mkdir -p t/$DIR	
+			 cp -frap  $EXPORTED_DIR/source/$f_path $EXPORTED_DIR/t/$DIR
+			if [ "$SIMULATION" == 1 ]
+			then
+				curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs --silent --show-error -T $f_path ftp://$FTP_SERVER$FTP_PATH$f_path
+			fi
+			continue;;
+
+		# STATUS: D (Deleted)
+		D )
+			# Check if it Is a file
+			echo $COLOR_LOG"DELETE... ($f_status): Trying to delete $f_path: \c"
+			if curl -u $FTP_USERNAME:$FTP_PASSWORD --output /dev/null --silent --head --fail ftp://$FTP_SERVER$FTP_PATH$f_path; then
+				if [ "$SIMULATION" == 1 ]
+				then
+					curl -u $FTP_USERNAME:$FTP_PASSWORD --output /dev/null --silent --show-error --quote "DELE $FTP_PATH$f_path" ftp://$FTP_SERVER
+                    echo "DELETED!";
+                else
+                    echo "DELETED! (SIMULATION, not really deleted)";
+				fi
+			else
+				echo "it does NOT EXISTS!"
+			fi
+			continue;;
+
+		# UNKNOWN STATUS! (TO DO...)
+		* )
+			echo $COLOR_ERROR"WARNING: I don't know this status: $f_status";
+			break;;
+	esac
+
+done < $EXPORTED_DIR$FILENAME_DIFF
+
+
+echo $COLOR_MSG"The commit $C_DEPLOY is deployed in PRODUCTION environment!"
+if [ "$SIMULATION" == 0 ]
+then
+	echo $COLOR_ALERT"** SIMULATION MODE ENABLED **"
+else
+	cd $PROJECT_REPO_DIR
+	git tag -f production $C_DEPLOY
+	echo $COLOR_MSG"Moved production tag to the commit '$C_DEPLOY'"
+	echo "Success!"
+fi
+# Reset sh color
+echo $COLOR_RESET
