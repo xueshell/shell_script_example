bc92611a84d37c3496d7c0c69623b9b1e7ff0a5f
Uahoo <uahoo@sina.com>
2014-07-01 18:25:30 +0800

    t

--- monitor/check_website_stats/monitor_website_with_curl.sh
+++ monitor/check_website_stats/monitor_website_with_curl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 smail() {
-mail -s "$1" gjw_apparitor@gmail.com <<EOF
+mail -s "$1" apparitor@gmail.com <<EOF
 $1
 $2
 ====
@@ -35,3 +35,18 @@ ssms ${Warning}
 smail CURL_Monitor ${Warning}
 fi
 
+
+#备注：
+#curl是一个命令行下的http下载工具，类似wget。与wget相似，它也可以通过发送指定的http header到服务器来判断服务的状态
+#这里介绍一个使用curl监控页面可用性的方法。
+#可以使用下面的命令，来采集页面的状态码。如果这条命令返回结果为200，说明服务正常。如果返回的是其他的页面，说明异常#。
+#curl -o /dev/null -s -w %{http_code} http://zys.8800.org/
+#-o 参数，是把下载的所有内容都重定向到/dev/null，-s命令，是屏蔽了curl本身的输出，而-w参数，是根据我们自己的需要，##自定义了curl的输出格式。
+#使用这条命令，再配合邮件和短信，就可以实现对页面的可用性监控。将这个程序部署在全国各地的机器上，就可以对cdn网络进#行可用性监控。
+#
+#curl只返回服务器响应状态，不返回内容，返回200是正常的，其它的不正常，简单的命令如下：
+#
+#[coomix@localhost ~]$ echo `curl -o /dev/null -s -m 10 --connect-timeout 10 -w %{http_code} "http://www.coomix.ne#t/index.jsp"`
+#200
+#[coomix@localhost ~]$ echo `curl -o /dev/null -s -m 10 --connect-timeout 10 -w %{http_code} "http://www.coomix.ne#t/index5.jsp"`
+#404
