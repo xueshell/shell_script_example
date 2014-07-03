9934a921c931e4d7d33fd7c0c12b0e64f14cc2f4
Uahoo <uahoo@sina.com>
2014-07-03 16:48:03 +0800

    t

--- shell_language/parsing_text_task/read_IFS.sh
+++ shell_language/parsing_text_task/read_IFS.sh
@@ -1,17 +1,20 @@
 #$ cat 1.txt
+cat << EOF >1.txt
 1,a
 2,b
 3,c
 4,d
-$ cat test.sh
+EOF
+#$ cat test.sh
 #!/bin/bash
 IFS=","
 while read a b
 do
         echo a=$a b=$b
 done < 1.txt
-$ sh test.sh 
-a=1 b=a
-a=2 b=b
-a=3 b=c
-a=4 b=d
+#$ sh test.sh 
+#a=1 b=a
+#a=2 b=b
+#a=3 b=c
+#a=4 b=d
+#unlink 1.txt
