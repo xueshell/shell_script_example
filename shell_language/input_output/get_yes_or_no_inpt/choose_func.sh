095d169e9a9411a595f8e68a4f99744538ff9917
Uahoo <uahoo@sina.com>
2014-07-14 18:52:51 +0800

    input

--- shell_language/input_output/get_yes_or_no_inpt/choose_func.sh
+++ shell_language/input_output/get_yes_or_no_inpt/choose_func.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-function choose{
+function choose {
 	
-	local default="$1"
-	local prompt="$2"
-	local choice_yes="$3"
-	local choice_no="$4"
+#	local default="$1"
+	local default="pls in put somethine (Y/N)?"
+#	local prompt="$2"
+	local prompt="Please input answer for this question (Y/N)?"
+	local choice_yes="$1"
+	local choice_no="$2"
 	local answer
 
 	read -p "$prompt" answer
 	[ -z "$answer" ] && answer="$default"
 
 	case "$answer" in
-		[yY] ) exec "$choice_yes"
+	#	[yY] ) exec "$choice_yes"
+		[yY] ) echo "$choice_yes"
 			#error check
 			;;
-		[nN] ) exec "$choice_no"
+#		[nN] ) exec "$choice_no"
+		[nN] ) echo "$choice_no"
 			#error check
 			;;
-		* ) printf "%b" "Unexpected answer '$answer!" >&2 ;;
+		* ) printf "%b  \n" "Unexpected answer '$answer!" >&2 ;;
 	esac
-} 
+}
+
+choose "$1" "$2" 
