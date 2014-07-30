ce167e74ceeca8ea83e125e6349525f35393ee70
Uahoo <uahoo@sina.com>
2014-07-30 17:55:27 +0800

    process jobs

--- shell_language/array/get_length_of_array.sh
+++ shell_language/array/get_length_of_array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## declare an array variable
-declare -a array=("one" "two" "three")
+declare -a array=("one" "two" "three" "four" "five" "lixin" "tim")
 
 # get length of an array
 arraylength=${#array[@]}
@@ -11,4 +11,20 @@ do
   echo $i " / " ${arraylength} " : " ${array[$i-1]}
 done
 
+####
+echo -e "\n"
+echo -e "\n"
+for k in ${array[@]}
+do
+   echo  $k 
+done
 #Output:
+
+#!/bin/bash
+# declare an array called array and define 3 vales
+files=( "/etc/passwd" "/etc/group" "/etc/hosts" )
+for i in "${files[@]}"
+do
+ #   echo $i
+ cat	 $i
+done
