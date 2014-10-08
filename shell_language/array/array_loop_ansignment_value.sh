26a9425ec9a3490f7cbdcdf4acef375026776089
lixin <lixin@staff.sina.com.cn>
2014-10-08 15:06:15 +0800

    array md5

--- shell_language/array/array_loop_ansignment_value.sh
+++ shell_language/array/array_loop_ansignment_value.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 declare -a NAME 
-n=0
+FName=""
+FPath=""
+Md5value=""
 FList=$(find . -type f )
-
+n=0
+m=0
 while read file
 do
 	
-	
-      	NAME[$n]=$(md5sum $file | cut -d ' ' -f1 )
-	n=$((n+1))	
+	FName=$( basename $file )
+	FPath=$( dirname $file )	
+      	Md5value=$(md5sum $file | cut -d ' ' -f1 )
+      	NAME[$n]="$FName,$FPath,$Md5value"
+	n=$(($n+1))	
 done<<EOF
 $FList
 EOF
 
-echo ${NAME[4]}
-echo ${NAME[8]}
-echo ${NAME[9]}
+
+
+while(( m<${#NAME[@]} ))
+do
+	echo ${NAME[m]}
+	m=$(($m+1))
+done
