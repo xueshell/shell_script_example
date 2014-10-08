a4bac87daf4af35f0bc10a9d71feae5869158f77
lixin <lixin@staff.sina.com.cn>
2014-10-08 18:34:18 +0800

    test finanle

--- shell_language/after_release_check_md5.sh
+++ shell_language/after_release_check_md5.sh
@@ -14,11 +14,11 @@ FList=$(find . -type f )
 
 create_md5() {
 
-	FName=$( basename $1 )
+#	FName=$( basename $1 )
 #	FPath=$( dirname $1 )	
       	MD5value=$(md5sum $1 | cut -d ' ' -f1 )
 #	echo  $FPath,$FName,$MD5value
-	echo  $FName:$MD5value
+	echo  $1:$MD5value
 }
 
 
@@ -67,13 +67,28 @@ file_md5_str(){
 	echo $CHeckFlist
 }
 
+uuid () {
+	
+	echo $(date +%s.%N)
+}
+return_check_blog_value(){
+	
+	clustername=""
+	mod=""	
+#	echo $str
+curl "http://dpadmin.grid.sina.com.cn/api/validist.php?clustername=blog&checklist=$(file_md5_str)&uuid=$(uuid)&mod=blog_sina_com_cn&igtime=60"
+}
+
+return_chack_detail(){
+
+	curl "http://dpadmin.grid.sina.com.cn/vapp/validist_recent.php?uuid=$(uuid)"
+}
 #cd $WOrkpath 
 
 file_md5_array
 	
-file_md5_str
+return_check_blog_value
 
+return_chack_detail
 
-
-#curl "http://dpadmin.grid.sina.com.cn/api/validist.php?clustername=blog&checklist=dpooltools/md5sum_v2.php:45ee210dee818f959623b980e23963c1,dist_test:d41d8cd98f00b204e9800998ecf8427e&uuid=1411474160.170537339&mod=int_dpool_sina_com_cn&igtime=60"
 ###
