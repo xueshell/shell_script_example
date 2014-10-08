f8c84ea7d97a3d6ad385c6ee4ce4b92af8c465f2
lixin <lixin@staff.sina.com.cn>
2014-10-08 14:06:21 +0800

    array

--- shell_language/t.sh
+++ shell_language/t.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 declare -a NAME 
 n=0
+FList=$(find . -type f )
 
-find . -type f | while read file
+while read file
 do
 	
 	
       	NAME[$n]=$(md5sum $file | cut -d ' ' -f1 )
 	n=$((n+1))	
-done
+done<<EOF
+$FList
+EOF
 
 echo ${NAME[4]}
+echo ${NAME[8]}
+echo ${NAME[9]}
