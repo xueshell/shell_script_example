c791274d653a1aff52330b5964a20597c6acec35
lixin <lixin@staff.sina.com.cn>
2014-11-14 18:18:13 +0800

    2014.11.14-18:18

--- work/src/detl.sh
+++ work/src/detl.sh
@@ -7,5 +7,10 @@ curl http://10.79.96.250/pub_check/view.php?check_uuid=1415948470.240327941 >/tm
 
 while  read LINE 
 do
-	echo $LINE | awk ' BEGIN { FS = "+"; ORS = ";   "   }  { print $3 } '
+ 	echo $LINE | awk ' BEGIN { FS = "+"; ORS = "\n"   }  { print $3 } '| awk 'ORS=NR%20?"\t":"\n"'
+#echo $LINE | awk ' BEGIN { FS = "+"; ORS = ";   "   }  { print $4 } '
+#awk 'ORS=NR%20?"\t":"\n"'
 done </tmp/$$ 
+
+
+rm -f /tmp/$$
