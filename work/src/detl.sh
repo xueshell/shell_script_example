30f25a9b652eac14e123cbac2568cad2aae70ef4
lixin <lixin@staff.sina.com.cn>
2014-12-18 15:04:09 +0800

    bakcup

--- work/src/detl.sh
+++ work/src/detl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl http://10.79.96.250/pub_check/view.php?check_uuid=1415948470.240327941 >/tmp/$$
+curl http://10.79.96.250/pub_check/view.php?check_uuid=1415948470.240327941 >/tmp/$$ 
 
 #cat /tmp/$$ | awk 'BEGIN { FS="\n"; } { print $2 }'
 
@@ -11,6 +11,5 @@ do
 #echo $LINE | awk ' BEGIN { FS = "+"; ORS = ";   "   }  { print $4 } '
 #awk 'ORS=NR%20?"\t":"\n"'
 done </tmp/$$ 
-
 
 rm -f /tmp/$$
